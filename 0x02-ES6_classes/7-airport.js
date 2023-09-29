266b929d32a26a537cc4ab4bb8400875c7d8a75e
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-09-29 01:31:17 +0100

    Airport class implemented

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -4,31 +4,7 @@ export default class Airport {
     this._code = code;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
-      throw new Error('Name must be a string');
-    }
-  }
-
-  set code(newCode) {
-    if (typeof newCode === 'string') {
-      this._code = newCode;
-    } else {
-      throw new Error('Code must be a string');
-    }
-  }
-
   toString() {
-    return this._code;
+    return `[object ${this._code}]`
   }
 }
