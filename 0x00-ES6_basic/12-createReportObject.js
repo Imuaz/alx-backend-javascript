c40c0c9142ff978bc64155c1ae96b166b2c02687
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-09-29 21:54:20 +0100

    report object created

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,16 +1,8 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  for (const departmentName in employeesList) {
-    if (Object.prototype.hasOwnProperty.call(employeesList, departmentName)) {
-      allEmployees[departmentName] = employeesList[departmentName];
-    }
-  }
-
-  const getNumberOfDepartments = () => Object.keys(allEmployees).length;
-
   return {
-    allEmployees,
-    getNumberOfDepartments,
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
   };
 }
