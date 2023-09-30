a51f0e93948a8ea2e0fd0e98de46b2ec5bd35d30
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-09-30 06:58:13 +0100

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const userResponse = results[1];
-      console.log('firstName:', userResponse.body.firstName);
-      console.log('lastName:', userResponse.body.lastName);
+
+    return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      const { body: photoBody } = photoResponse;
+      const { firstName, lastName } = userResponse.body;
+      console.log(`${photoBody} ${firstName} ${lastName}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
 }
