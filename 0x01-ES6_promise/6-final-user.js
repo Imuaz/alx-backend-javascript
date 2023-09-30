59bc7009a4663aea90979018030bdc400743b80a
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-09-30 08:18:33 +0100

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const resultsArray = [];
+
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results);
+    .then((results) => {
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          resultsArray.push({ status: result.status, value: result.value });
+        } else {
+          resultsArray.push({ status: result.status, value: `Error: ${result.reason.message}` });
+        }
+      });
+      return resultsArray;
+    });
 }
