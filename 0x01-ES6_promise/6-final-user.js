1078b127312169aaa03288649b617a7db9aa666f
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-09-30 08:12:29 +0100

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,13 +2,6 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => (
-    results.map((result) => ({
-      status: result.status === 'fulfilled' ? 'fulfilled' : 'rejected',
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }))
-  ));
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results);
 }
