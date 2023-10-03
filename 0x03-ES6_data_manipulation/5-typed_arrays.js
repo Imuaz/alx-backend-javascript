9377804ba93be667ad69c2bb6c4d5021aaa10be8
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-03 15:46:08 +0100

    Typed arrays function created

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,9 +4,9 @@ export default function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
+  const dataView = new DataView(buffer);
 
-  int8Array[position] = value;
+  dataView.setInt8(position, value);
 
-  return buffer;
+  return dataView;
 }
