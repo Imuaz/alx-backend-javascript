c2a2ac333d2966b017da540a3330b6bd5dc09586
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-03 20:18:30 +0100

    Clean set function

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,14 @@
 export default function cleanSet(inputSet, startString) {
-  if (typeof startString === 'string' || startString.length !== 0) {
-    const setArray = Array.from(inputSet);
-    const filteredValues = setArray
-	  .filter(value => value && value.startsWith(startString))
-	  .map(value => value.slice(startString.length));
+  if (startString === '' || !(typeof startString === 'string')) return '';
+  const filteredValues = [];
 
-    const resultString = filteredValues.join('-');
-    return resultString;
+  for (const value of inputSet) {
+    if (value.startsWith(startString)) {
+      filteredValues.push(value.slice(startString.length));
+    }
   }
-  return '';  
+
+  const resultString = filteredValues.join('-');
+
+  return resultString;
 }
