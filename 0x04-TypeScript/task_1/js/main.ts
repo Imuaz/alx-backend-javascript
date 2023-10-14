80317bc704eff3c5cff14f568b637d25e0a56bc7
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-10-14 23:52:26 +0100

    finish some tasks

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -2,15 +2,15 @@ export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  yearsOfExperience: number;
+  yearsOfExperience?: number;
   location: string;
-  [key: string]: any;
+  [propName: string]: any;
 };
 
 const teacher3: Teacher = {
-  firstname: 'John',
-  fulltimeemployee: false,
-  lastname: 'Doe',
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
   location: 'London',
   contract: false
 };
@@ -23,11 +23,56 @@ interface Directors extends Teacher {
 }
 
 const director1: Directors = {
-  firstname: 'John',
-  lastname: 'Doe',
+  firstName: 'John',
+  lastName: 'Doe',
   location: 'London',
-  fulltimeemployee: true,
-  numberofreports: 17
+  fullTimeEmployee: true,
+  numberOfReports: 17
 };
 
 console.log(director1);
+
+// function that returns name Innitials and it interface
+export function printTeacher(firstName: string, lastName:string): string {
+  const firstLetter = firstName.charAt(0);
+  return `${firstLetter} ${lastName}`;
+}
+
+interface priprintTeacherFunction{
+  printTeacher(firstName: string, lastName: string): string;
+}
+
+// class `StudentClass`
+export interface StudentClassInterface {
+  displayName(): string;
+  workOnHomeWork(): string
+};
+
+export interface StudentConstructor {
+  new(constructorArgs: {firstName: string, lastName: string}): StudentClassInterface
+}
+
+export class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(arg: {firstName: string, lastName: string}) {
+    this.firstName = arg.firstName;
+    this.lastName = arg.lastName;
+  }
+
+  workOnHomeWork(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+//testing my StudentClass
+// const student: StudentConstructor = StudentClass;
+// const newStudent = new StudentClass({firstName: 'Julien', lastName: 'Barbier'})
+// console.log(newStudent)
+// console.log(newStudent.displayName())
+// console.log(newStudent.workOnHomeWork())
---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,75 @@
+//creating interface `DirectorInterfece`
+export interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+};
+
+//creating interface `TeacherInterface`
+export interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
+};
+
+//Director class that implements DirectorInterface
+export const Director = class Director implements DirectorInterface {
+
+    workFromHome(): string {
+        return 'Working from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Getting a coffee break';
+    }
+    workDirectorTasks(): string {
+        return 'Getting to director tasks'
+    }
+}
+
+//Teacher class that implements TeacherInterface
+const Teacher = class Teacher implements TeacherInterface {
+    
+    workFromHome(): string {
+        return 'Cannot work from home';
+    }
+
+    getCoffeeBreak(): string {
+        return 'Cannot have a break';
+    }
+
+    workTeacherTasks(): string {
+        return 'Getting to work';
+    }
+}
+
+export function createEmployee(salary: number | string): DirectorInterface | TeacherInterface {
+    if (typeof salary  !== 'string' && salary < 500) {
+        return new Teacher;
+    }
+
+    return new Director;
+}
+
+// console.log(createEmployee(200));
+// console.log(createEmployee(1000));
+// console.log(createEmployee('$500'));
+
+
+//creating isDirector function
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is DirectorInterface {
+    return (employee as DirectorInterface).workDirectorTasks !== undefined;
+}
+
+//creating function `executework`
+export function executeWork(employee: DirectorInterface | TeacherInterface): string | null {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    } else {
+        return employee.workTeacherTasks();
+    }
+}
+
+console.log(executeWork(createEmployee(200)));
+console.log(executeWork(createEmployee(1000)));
+
