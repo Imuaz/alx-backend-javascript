f0c6b27d6661f56dfd69c7d8bb05b5d22da865e3
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-22 17:30:45 +0100

    create small http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,8 +5,8 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname)
