14a1da5b907f8dc9b4b3a774889e95fbb401bd4c
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-22 21:45:11 +0100

    Create a more complex HTTP server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,32 +1,27 @@
 const fs = require('fs').promises;
 const http = require('http');
 
-function countStudents(filepath) {
-  return fs.readFile(filepath, 'utf-8')
-    .then((data) => {
-      const students = data
-        .split('\n')
-        .map((student) => student.split(','))
-        .filter((student) => student.length === 4 && student[0] !== 'firstname')
-        .map((student) => ({ name: student[0], field: student[3] }));
+async function countStudents(filepath) {
+  try {
+    const data = await fs.readFile(filepath, 'utf-8');
+    const students = data
+      .split('\n')
+      .map((student) => student.split(','))
+      .filter((student) => student.length === 4 && student[0] !== 'firstname')
+      .map((student) => ({ name: student[0], field: student[3] }));
 
-      const getStudentsByField = (field) => students
-        .filter((student) => student.field === field)
-        .map((student) => student.name);
+    const getStudentsByField = (field) => students
+      .filter((student) => student.field === field)
+      .map((student) => student.name);
 
-      const numberOfStudents = students.length;
-      const csStudents = getStudentsByField('CS');
-      const sweStudents = getStudentsByField('SWE');
-
-      return {
-        numberOfStudents,
-        csStudents,
-        sweStudents,
-      };
-    })
-    .catch(() => {
-      throw new Error('Cannot load the database');
-    });
+    return {
+      numberOfStudents: students.length,
+      csStudents: getStudentsByField('CS'),
+      sweStudents: getStudentsByField('SWE'),
+    };
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 const app = http.createServer(async (req, res) => {
