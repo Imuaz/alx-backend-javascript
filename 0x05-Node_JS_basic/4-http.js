04b1be7e3fb51bfe01a018cd8ac821e8df193ae0
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-22 17:16:19 +0100

    create small http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,12 +3,15 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
-});
+const serverHandler = (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Holberton School!\n');
+};
 
-app.listen(port, hostname);
+const app = http.createServer(serverHandler);
+
+app.listen(port, hostname, () => {
+  console.log(`Server is listening on http://${hostname}:${port}`);
+});
 
 module.exports = app;
