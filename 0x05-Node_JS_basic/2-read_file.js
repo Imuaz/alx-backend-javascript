fe809a72de73030eef83a0f89f18e422804c5f5b
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-22 16:04:18 +0100

    fixes some issue

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+function countStudents(filepath) {
+  try {
+    const data = fs.readFileSync(filepath, 'utf-8');
+    
+    const students = data
+      .split('\n')
+      .map((student) => student.split(','))
+      .filter((student) => student.length === 4 && student[0] !== 'firstname')
+      .map((student) => ({ name: student[0], field: student[3] }));
+
+    const getStudentsByField = (field) => students
+      .filter((student) => student.field === field)
+      .map((student) => student.name);
+
+    const csStudents = getStudentsByField('CS');
+    const sweStudents = getStudentsByField('SWE');
+
+    console.log(`Number of students: ${students.length}`);
+    console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
