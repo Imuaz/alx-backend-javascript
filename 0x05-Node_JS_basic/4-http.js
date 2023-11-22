596632cbddbb5460185be5439ebd356ff8df4be1
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-22 17:09:21 +0100

    create small http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,17 +1,12 @@
 const http = require('http');
 
-// Create the HTTP server
 const app = http.createServer((req, res) => {
-  // Set the content type to plain text
-  res.setHeader('Content-Type', 'text/plain');
-  // Send the "Hello Holberton School!" message
+  res.writeHead(200, { 'Content-Type', 'text/plain' });
   res.end('Hello Holberton School!\n');
 });
 
 // Make the server listen on port 1245
 const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-// Export the app variable
+app.listen(PORT, 'localhost')
+
 module.exports = app;
