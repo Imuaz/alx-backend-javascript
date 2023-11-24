d2352ea30e01e780c5153335e39651eeff76c167
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-25 00:18:10 +0100

    Regex integration testing

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,35 +2,59 @@ const request = require('request');
 const { describe, it } = require('mocha');
 const { expect } = require('chai');
 
-describe('Cart page', () => {
-  const baseUrl = 'http://localhost:7865/cart/';
+const baseUrl = 'http://localhost:7865/';
 
-  const makeRequest = (id) => {
-    const url = `${baseUrl}${id}`;
-    return new Promise((resolve) => {
-      request(url, (err, res, body) => {
-        resolve({ err, res, body });
-      });
-    });
+const makeRequest = (url, callback) => {
+  request.get(url, (err, res, body) => {
+    callback(err, res, body);
+  });
+};
+
+describe('Index page', () => {
+  const indexOptions = {
+    url: baseUrl,
+    method: 'GET',
   };
 
-  it('checks correct status code when :id is a number', async () => {
-    const { res } = await makeRequest(123);
-    expect(res.statusCode).to.equal(200);
+  it('checks correct status code', (done) => {
+    request(indexOptions, (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
   });
 
-  it('checks correct content when :id is a number', async () => {
-    const { body } = await makeRequest(123);
-    expect(body).to.contain('Payment methods for cart 123');
+  it('checks correct content', (done) => {
+    request(indexOptions, (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart page', () => {
+  const cartOptions = (id) => ({
+    url: `${baseUrl}cart/${id}`,
+    method: 'GET',
   });
 
-  it('checks correct status code when :id is NOT a number (404)', async () => {
-    const { res } = await makeRequest('invalid');
-    expect(res.statusCode).to.equal(404);
+  it('checks correct status code for correct url', (done) => {
+    makeRequest(cartOptions('12'), (err, res) => {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
   });
 
-  it('checks correct content when :id is NOT a number (404)', async () => {
-    const { body } = await makeRequest('invalid');
-    expect(body).to.contain('Cannot GET /cart/invalid');
+  it('checks correct content for correct url', (done) => {
+    makeRequest(cartOptions('12'), (err, res, body) => {
+      expect(body).to.contain('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('checks correct status code for incorrect url', (done) => {
+    makeRequest(cartOptions('kim'), (err, res) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
   });
 });
