89ae5041e4bb0139693ae43ac18c327edd962edf
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-11-25 08:41:05 +0100

    fixes the lint error

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,23 @@
+const { readFile } = require('fs').promises;
+                                                     module.exports = function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {              readFile(filePath, 'utf-8')
+      .then((data) => {
+        const lines = data.split('\n').slice(1);             const students = lines.reduce((acc, line) => {
+          if (line) {
+            const [name, , , field] = line.split(',').map((item) => item.trim());
+
+            if (!acc[field]) {
+              acc[field] = [name];
+            } else {
+              acc[field].push(name);
+            }
+          }
+          return acc;
+        }, {});
+        resolve(students);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
